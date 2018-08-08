e2197f1c787743b79b1a6a4dd8237d205cfea5ac
Mihail Ciobanu <positronmihai@gmail.com>
2018-08-08 14:41:17 +0300

    Ready Eddy

--- src/components/Navigation/NavList/index.spec.js
+++ src/components/Navigation/NavList/index.spec.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from 'react'
 
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-import NavList from './NavList';
-import NavItems from './NavItems/NavItems';
+import NavList from './NavList'
+import NavItems from './NavItems/NavItems'
 
-configure({adapter: new Adapter()});
+configure({adapter: new Adapter()})
 
 describe('<NavList />', () => {
-  let wrapper;
+  let wrapper
 
   beforeEach(() => {
-    wrapper = shallow(<NavList />);
-  });
+    wrapper = shallow(<NavList />)
+  })
 
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    expect(wrapper.find(NavItems)).toHaveLength(2);
-  });
+    expect(wrapper.find(NavItems)).toHaveLength(2)
+  })
 
   it('should render three <NavigationItem /> elements if authenticated', () => {
     // wrapper = shallow(<NavigationItems isAuthenticated />);
     wrapper.setProps({isAuthenticated: true});
-    expect(wrapper.find(NavItems)).toHaveLength(3);
-  });
+    expect(wrapper.find(NavItems)).toHaveLength(3)
+  })
 
   it('should an exact logout button', () => {
-    wrapper.setProps({isAuthenticated: true});
-    expect(wrapper.contains(<NavItems link="/logout">Logout</NavItems>)).toEqual(true);
-  });
-});
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.contains(<NavItems link="/logout">Logout</NavItems>)).toEqual(true)
+  })
+})
