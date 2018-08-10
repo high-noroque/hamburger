c8512191f99d529defdb91a9ef6536246c4d42c0
Mihail Ciobanu <positronmihai@gmail.com>
2018-08-10 15:24:02 +0300

    Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes'
-import axios from 'axios'
 
 export const authStart = () => {
   return {
@@ -23,47 +22,33 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
-  localStorage.removeItem('token')
-  localStorage.removeItem('expirationDate')
-  localStorage.removeItem('userId')
+  // localStorage.removeItem('token')
+  // localStorage.removeItem('expirationDate')
+  // localStorage.removeItem('userId')
   return {
-    type: actionTypes.AUTH_LOGOUT
+    type: actionTypes.AUTH_INITIATE_LOGOUT
   }
 }
 
-export const checkAuthTimeout = expirationDate => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(logout())
-    }, expirationDate * 1000)
+export const logoutSucceed = () => {
+  return {
+    type: actionTypes.AUTH_INITIATE_LOGOUT
+  }
+}
+
+export const checkAuthTimeout = expirationTime => {
+  return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    expirationTime: expirationTime
   }
 }
 
 export const auth = (email, password, isSignedUp) => {
-  return dispatch => {
-    dispatch(authStart())
-    const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }
-    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCTwtXqoS221oK5M1fCTGiRmgULDAC0WTk'
-    if(!isSignedUp) {
-      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCTwtXqoS221oK5M1fCTGiRmgULDAC0WTk'
-    }
-    axios.post(url, authData)
-      .then(response => {
-        // console.log(response)
-        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
-        localStorage.setItem('token', response.data.idToken)
-        localStorage.setItem('expirationData', expirationDate)
-        localStorage.setItem('userId', response.data.localId)
-        dispatch(authSuccess(response.data.idToken, response.data.localId))
-        dispatch(checkAuthTimeout(response.data.expiresIn))
-      })
-      .catch(err => {
-        dispatch(authFail(err.response.data.error))
-      })
+  return {
+    type: actionTypes.AUTH_USER,
+    email: email,
+    password: password,
+    isSignedUp: isSignedUp
   }
 }
 
@@ -75,19 +60,7 @@ export const setAuthRedirectPath = path => {
 }
 
 export const authCheckState = () => {
-  return dispatch => {
-    const token = localStorage.getItem('token')
-    if (!token) {
-      dispatch(logout())
-    } else {
-      const expirationDate = new Date(localStorage.getItem('expirationDate'))
-      if (expirationDate <= new Date()) {
-        dispatch(logout())
-      } else {
-        const userId = localStorage.getItem('userId')
-        dispatch(authSuccess(token, userId))
-        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime() / 1000)))
-      }
-    }
+  return {
+    type: actionTypes.AUTH_CHECK_STATE
   }
 }
